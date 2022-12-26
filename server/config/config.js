b649428b15defb31d4a117964e26d6198aa729fd
German Ojeda <germanojeda83@gmail.com>
2022-12-26 13:23:39 -0300

    Backend ok

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,12 @@
 //Puerto
 //======================
 
-process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 8000;
 
 //======================
 //Entorno
 //======================
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'de';
 
 //======================
 //Base de datos
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV == 'dev') {
     URLDB = 'mongodb://localhost/territorios';
 } else {
     // URLDB = process.env.BBDD;
-    URLDB = 'mongodb+srv://teltrun:Italia3003@cluster0-6feoa.mongodb.net/territorios?retryWrites=true&w=majority'
+    URLDB = 'mongodb+srv://teltrun:Italia3003@cluster0-6feoa.mongodb.net/territorios?retryWrites=true';
 
 }
 
